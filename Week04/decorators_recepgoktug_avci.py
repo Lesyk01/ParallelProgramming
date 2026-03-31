76e00a301a9c4073a7fd3e24d77949f951b5f499
Recep Göktuğ Avcı <qcmnjcdqv2@privaterelay.appleid.com>
2026-03-31 06:37:26 +0300

    Week04_decorators_by_goktugavci

---
+++ Week04/decorators_recepgoktug_avci.py
@@ -0,0 +1,24 @@
+custom_power = lambda x=0, /, e=1: x ** e
+
+
+def custom_equation(x: int = 0, y: int = 0, /,
+                    a: int = 1, b: int = 1, *,
+                    c: int = 1) -> float:
+
+
+    for val in (x, y, a, b, c):
+        if not isinstance(val, int):
+            raise TypeError("Arguments must be integers")
+
+    return float((x ** a + y ** b) / c)
+
+
+def fn_w_counter() -> (int, dict[str, int]):
+    if not hasattr(fn_w_counter, "count"):
+        fn_w_counter.count = 0
+
+    fn_w_counter.count += 1
+
+    module_name = __name__.split('.')[-1]
+
+    return fn_w_counter.count, {module_name: fn_w_counter.count}
