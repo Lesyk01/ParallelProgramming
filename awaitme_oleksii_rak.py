3b3b8f864729ae06e1c019f114650e21243861ea
Lesyk01 <olexua10@gmail.com>
2026-04-05 15:55:28 +0300

    Create awaitme_oleksii_rak.py

---
+++ awaitme_oleksii_rak.py
@@ -0,0 +1,13 @@
+import inspect
+from functools import wraps
+
+def awaitme(func):
+
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if inspect.isawaitable(result):
+            return await result
+        return result
+
+    return wrapper
