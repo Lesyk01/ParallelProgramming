293ce8e2430bcfd208013a84c0f8671bf3e0176c
Senkiv Vladyslav <vladyslav.senkiv@gmail.com>
2026-04-05 16:15:57 +0300

    Create timer_vladyslav_senkiv.py

---
+++ Week06/timer_vladyslav_senkiv.py
@@ -0,0 +1,15 @@
+from time import perf_counter
+
+class Timer:
+
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def __enter__(self):
+        self.start_time = perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end_time = perf_counter()
+        return False
