bff240dd2d7fa0b16b013b7aa2fd05a8df966bb6
afrainanc <afrainanc@gmail.com>
2026-04-07 22:25:08 +0300

    Update timer_afra_inanc.py

--- Week06/timer_afra_inanc.py
+++ Week06/timer_afra_inanc.py
@@ -1,13 +1,13 @@
 import time
 
 class Timer:
-    def _init_(self):
+    def __init__(self):
         self.start_time = None
         self.end_time = None
 
-    def _enter_(self):
+    def __enter__(self):
         self.start_time = time.time()
         return self
 
-    def _exit_(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.end_time = time.time()
