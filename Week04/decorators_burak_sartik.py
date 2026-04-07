52a068bc4a5f3a2306329aad0f6c136f55490d84
Burak Sartık <sartikb@outlook.com>
2026-04-07 17:58:25 +0300

    Update decorators_burak_sartik.py

--- Week04/decorators_burak_sartik.py
+++ Week04/decorators_burak_sartik.py
@@ -2,24 +2,26 @@ import time
 import tracemalloc
 
 def performance(func):
+    if not hasattr(performance, 'counter'):
+        performance.counter = 0
+        performance.total_time = 0.0
+        performance.total_mem = 0.0
     def wrapper(*args, **kwargs):
         tracemalloc.start()
-        t1 = time.perf_counter()
-
+        start_time = time.perf_counter()
+        
         result = func(*args, **kwargs)
-
-        t2 = time.perf_counter()
-        _, peak = tracemalloc.get_traced_memory()
+        
+        end_time = time.perf_counter()
+        current_mem, peak_mem = tracemalloc.get_traced_memory()
         tracemalloc.stop()
-
+        
         performance.counter += 1
-        performance.total_time += (t2 - t1)
-        performance.total_mem += peak
-
+        performance.total_time += (end_time - start_time)
+        performance.total_mem += peak_mem
+        
         return result
-
+    wrapper.__name__ = func.__name__
+    wrapper.__doc__ = func.__doc__
+    
     return wrapper
-
-performance.counter = 0
-performance.total_time = 0
-performance.total_mem = 0
