628e6a348a1cfba7022cf21947d8fae26d2331fc
Senkiv Vladyslav <vladyslav.senkiv@gmail.com>
2026-06-03 15:17:18 +0300

    fix

--- Week04/functions_vladyslav_senkiv.py
+++ Week04/functions_vladyslav_senkiv.py
@@ -11,23 +11,23 @@ def custom_equation(
     c: int = 1
 ) -> float:
     """
-    Calculates the equation (a*x + b*y) / c.
+    Calculates custom equation.
 
-    :param x: first integer value
-    :param y: second integer value
-    :param a: multiplier for x
-    :param b: multiplier for y
-    :param c: divisor value
-    :return: result of the equation
+    :param x: first number
+    :param y: second number
+    :param a: coefficient for x
+    :param b: coefficient for y
+    :param c: divisor
+    :return: result of equation
     """
 
     values = [x, y, a, b, c]
 
     for value in values:
         if not isinstance(value, int):
-            raise TypeError("All parameters must be integers")
+            raise TypeError("value must be int")
 
-    return (a * x + b * y) / c
+    return (x * a + y * b) / c
 
 
 counter = 0
@@ -37,6 +37,5 @@ def fn_w_counter() -> (int, dict[str, int]):
     global counter
 
     counter += 1
-    module_name = __name__
 
-    return counter, {module_name: counter}
+    return counter, {__name__: counter}
