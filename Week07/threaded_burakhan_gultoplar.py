34590e3b6960f94deae10acb5bb79c0291e6b624
Burakhan7 <141933959+Burakhan7@users.noreply.github.com>
2026-06-04 22:24:28 +0300

    Update threaded_burakhan_gultoplar.py

--- Week07/threaded_burakhan_gultoplar.py
+++ Week07/threaded_burakhan_gultoplar.py
@@ -5,15 +5,15 @@ def eszamanli_calistir(thread_sayisi: int):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            is parçacıkları = [
+            is parcaciklari = [
                 threading.Thread(target=func, args=args, kwargs=kwargs)
                 for _ in range(thread_sayisi)
             ]
             
-            for t in is_parçacıkları:
+            for t in is_parcaciklari:
                 t.start()
                 
-            for t in is_parçacıkları:
+            for t in is_parcaciklari:
                 t.join()
                 
         return wrapper
